cb815a5498ad481bbbe3f14a7d4602345d955ed3
pmisobl <pmisobl@gmail.com>
2025-02-20 22:54:00 +0100

    bla

---
+++ ex5.py
@@ -0,0 +1,22 @@
+my_name = 'Mihajlo Previsic'
+my_age = 38 # not a lie 
+my_height = 181 # CM
+my_weight = 100 # kg 
+my_eyes = "green" 
+my_teeth = 'white'
+my_hair = "bald"
+print (f"Lets talk about {my_name}")
+print (f"He is {my_age} years old")
+print (f"He is {my_weight} kilos heavy")
+print (f"That's to much for his height whis is {my_height} cm")
+print (f"He is got {my_eyes} eyes and {my_hair} hair")
+print (f"His teeth are usualy {my_teeth}")
+# tricky line 
+total = my_age + my_height + my_weight
+print (f"If i add {my_age} , {my_height} and {my_weight}, ill get {total}")
+
+inch_height= my_height * 0.39
+print (f"he have {inch_height} height in inches")
+
+pound_weight = my_weight * 2.204
+print (f"He have {pound_weight} weight in pounds")
