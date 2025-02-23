e0a620aabdfa19398da3ea4b4ff99a23e5a9e70e
pmisobl <pmisobl@gmail.com>
2025-02-23 16:58:54 +0100

    ex10 i 11

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print ("How old are you", end = " ")
+age = input()
+print ("How tall is you", end = ' ')
+height = input()
+print ("How much do you weight" , end =' ')
+weight = input()
+
+print (f"So, you're {age} years old, {height} cmd tall, and {weight} kg heavy")
---
+++ ex9.py
@@ -0,0 +1,13 @@
+#Here is some new strange stuff, remember to type it exactly
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\n Jan\n Feb \n Mar \n Apr \n May \n Jun \n Jul \n Aug " 
+
+print ( "here are the days:", days )
+print (" Here are the months:", months)
+print ("\n")
+print (""" There's something odd here 
+       With the three doubled quotes. 
+       We'll be able to to type as much as we like. 
+       Even 4 lines if we want, or 5, or 6. 
+        """)
